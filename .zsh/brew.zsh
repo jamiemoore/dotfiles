7d247b0567aa28a0c231440d6fc6e4320f937a33
Jamie Moore <jbgmoore@gmail.com>
2024-12-27 17:09:19 +1100

    brew variable now exporting

--- .zsh/brew.zsh
+++ .zsh/brew.zsh
@@ -7,7 +7,7 @@ if [[ ! -f ~/.cache/brew_prefix ]]; then
     mkdir -p ~/.cache
     brew --prefix > ~/.cache/brew_prefix
 fi
-readonly HOMEBREW=$(<~/.cache/brew_prefix)
+export HOMEBREW=$(<~/.cache/brew_prefix)
 
 # Cache brew shellenv output
 if [[ ! -f ~/.cache/brew_shellenv ]] || [[ ! -s ~/.cache/brew_shellenv ]]; then
